369d567c4483d2cc38e88337eef9b5484c7a9aa4
Manuel <manuel-mesas.g@hotmail.com>
2019-05-29 22:04:45 +0200

    Update Audio.js

--- src/Audio.js
+++ src/Audio.js
@@ -3,19 +3,19 @@ class Audio extends THREE.Object3D {
   constructor () {
     super();
     this.fondo = new Howl({
-      src: ['../sounds/ambient.ogg'], volume: 0.35, loop: true, 
+      src: ['../sounds/ambient.ogg'], volume: 0.35, loop: true, preload: true
     });
 
     this.boom = new Howl({
-      src: ['../sounds/impact.mp3'], volume: 0.9, loop: false
+      src: ['../sounds/impact.mp3'], volume: 0.9, loop: false, preload: true
     });
 
     this.gameOver = new Howl({
-      src: ['../sounds/GameOver.ogg'], volume: 0.2, loop: false
+      src: ['../sounds/GameOver.ogg'], volume: 0.2, loop: false, preload: true
     });
 
     this.risa = new Howl({
-      src: ['../sounds/laugh.ogg'], volume: 0.4, loop: false
+      src: ['../sounds/laugh.ogg'], volume: 0.4, loop: false, preload:true
     });
 
     //this.fondo.play();
